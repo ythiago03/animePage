fe1c381a678a99f21d9a40b4cac9362ea273d71a
Thiago Henrique <thiagohcfidencio@gmail.com>
2022-10-27 10:26:06 -0300

    Inital commit

---
+++ assets/js/cardsList.js
@@ -0,0 +1,195 @@
+export const cardList =
+[
+    {
+        name: 'Chainsaw Man',
+        genre: 'Ação',
+        ranking: 9.64,
+        studio: 'Mappa',
+        linkCard: 'https://myanimelist.net/anime/44511/Chainsaw_Man',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1806/126216.jpg',
+    },
+    {
+        name: 'Spy x Family Part 2',
+        genre: 'Comédia',
+        ranking: 8.83,
+        studio: 'Wit Studio',
+        linkCard: 'https://myanimelist.net/anime/50602/Spy_x_Family_Part_2',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1111/127508.jpg',
+    },
+    {
+        name: 'Plastic Memories',
+        genre: 'Drama',
+        ranking: 7.91,
+        studio: 'Doga Kobo',
+        linkCard: 'https://myanimelist.net/anime/27775/Plastic_Memories?q=Plastic&cat=anime',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/4/72750.jpg',
+    },
+    {
+        name: 'The Eminence in Shadow',
+        genre: 'Ecchi',
+        ranking: 7.55,
+        studio: 'Nexus',
+        linkCard: 'https://myanimelist.net/anime/48316/Kage_no_Jitsuryokusha_ni_Naritakute',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1874/121869.jpg',
+    },
+    {
+        name: "Komi Can't Communicate Season 2",
+        genre: 'Slice of Life',
+        ranking: 8.13,
+        studio: 'OLM',
+        linkCard: 'https://myanimelist.net/anime/50631/Komi-san_wa_Comyushou_desu_2nd_Season?q=komi%20&cat=anime',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1108/121157.jpg',
+    },
+    {
+        name: 'Another',
+        genre: 'Terror',
+        ranking: 7.49,
+        studio: 'P.A. Works',
+        linkCard: 'https://myanimelist.net/anime/11111/Another',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/4/75509.jpg',
+    },
+    {
+        name: 'Saga of Tanya the Evil',
+        genre: 'Militar',
+        ranking: 7.97,
+        studio: 'Nut',
+        linkCard: 'https://myanimelist.net/anime/32615/Youjo_Senki?q=saga&cat=anime',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/5/82890.jpg',
+    },
+    {
+        name: 'Dr. Stone',
+        genre: 'Ficção Científica',
+        ranking: 8.29,
+        studio: 'TMS Entertainment',
+        linkCard: 'https://myanimelist.net/anime/38691/Dr_Stone',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1613/102576.jpg',
+    },
+    {
+        name: 'Rascal Does Not Dream of Bunny Girl Senpai',
+        genre: 'Romance',
+        ranking: 8.26,
+        studio: 'CloverWorks',
+        linkCard: 'https://myanimelist.net/anime/37450/Seishun_Buta_Yarou_wa_Bunny_Girl_Senpai_no_Yume_wo_Minai',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1301/93586.jpg',
+    },
+    {
+        name: 'Mushoku Tensei: Jobless Reincarnation Part 2',
+        genre: 'Isekai',
+        ranking: 8.72,
+        studio: 'Studio Bind',
+        linkCard: 'https://myanimelist.net/anime/45576/Mushoku_Tensei__Isekai_Ittara_Honki_Dasu_Part_2?q=mushoku%20tensei&cat=anime',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1028/117777.jpg',
+    },
+    {
+        name: 'Jujutsu Kaisen',
+        genre: 'Ação',
+        ranking: 8.67,
+        studio: 'Mappa',
+        linkCard: 'https://myanimelist.net/anime/40748/Jujutsu_Kaisen',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1171/109222.jpg',
+    },
+    {
+        name: 'Kaguya-sama: Love is War',
+        genre: 'Comédia',
+        ranking: 8.41,
+        studio: 'A-1 Pictures',
+        linkCard: 'https://myanimelist.net/anime/37999/Kaguya-sama_wa_Kokurasetai__Tensai-tachi_no_Renai_Zunousen',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1295/106551.jpg',
+    },
+    {
+        name: 'Anohana: The Flower We Saw That Day',
+        genre: 'Drama',
+        ranking: 8.32,
+        studio: ' A-1 Pictures',
+        linkCard: 'https://myanimelist.net/anime/9989/Ano_Hi_Mita_Hana_no_Namae_wo_Bokutachi_wa_Mada_Shiranai',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/5/79697.jpg',
+    },
+    {
+        name: 'No Game No Life',
+        genre: 'Ecchi',
+        ranking: 8.10,
+        studio: ' Madhouse',
+        linkCard: 'https://myanimelist.net/anime/19815/No_Game_No_Life',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1074/111944.jpg',
+    },
+    {
+        name: "Miss Kobayashi's Dragon Maid S",
+        genre: 'Slice of Life',
+        ranking: 8.34,
+        studio: 'Kyoto Animation',
+        linkCard: 'https://myanimelist.net/anime/39247/Kobayashi-san_Chi_no_Maid_Dragon_S',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1252/115539.jpg',
+    },
+    {
+        name: 'Tokyo Ghoul',
+        genre: 'Terror',
+        ranking: 7.79,
+        studio: 'Pierrot',
+        linkCard: 'https://myanimelist.net/anime/22319/Tokyo_Ghoul',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/5/64449.jpg',
+    },
+    {
+        name: '86 Eighty Six',
+        genre: 'Militar',
+        ranking: 8.26,
+        studio: 'A-1 Pictures',
+        linkCard: 'https://myanimelist.net/anime/41457/86',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1987/117507.jpg',
+    },
+    {
+        name: 'Made in Abyss',
+        genre: 'Ficção Científica',
+        ranking: 8.67,
+        studio: 'Kinema Citrus',
+        linkCard: 'https://myanimelist.net/anime/34599/Made_in_Abyss',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/6/86733.jpg',
+    },
+    {
+        name: 'Horimiya',
+        genre: 'Romance',
+        ranking: 8.20,
+        studio: 'CloverWorks',
+        linkCard: 'https://myanimelist.net/anime/42897/Horimiya',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1695/111486.jpg',
+    },
+    {
+        name: 'Re:Zero kara Hajimeru Isekai Seikatsu',
+        genre: 'Isekai',
+        ranking: 8.24,
+        studio: 'White Fox',
+        linkCard: 'https://myanimelist.net/anime/31240/Re_Zero_kara_Hajimeru_Isekai_Seikatsu?q=Re%3A&cat=anime',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1522/128039.jpg',
+    },
+]         
+   
+/*
+const URL = 'https://kitsu.io/api/edge/trending/anime'
+const list = [
+    {
+        name: 'Chainsaw Man',
+        genre: 'Ação',
+        ranking: 9.64,
+        studio: 'Mappa',
+        linkCard: 'https://myanimelist.net/anime/44511/Chainsaw_Man',
+        urlCard: 'https://cdn.myanimelist.net/images/anime/1806/126216.jpg',
+    }
+]
+
+
+const api = async url => {
+
+    try{
+        const response = await fetch(url)
+            .then(response => response.json())
+            .then(data => fetch(`https://kitsu.io/api/edge/${data.data[0].relationships.genres.links.self}`).json()
+                .then(res => console.log(res)))
+      
+    }
+    catch(err){
+        console.log(err);
+    }
+} 
+
+api(URL)
+
+*/
---
+++ assets/js/main.js
@@ -0,0 +1,203 @@
+import {cardList} from './cardsList.js';
+
+/* MENU ANIMATION */
+
+/* getting elements */
+const openProfile = document.querySelector('.profile')
+const profileSec = document.querySelector('.profileFull')
+const closeProfile = document.querySelector('.closeProfile')
+const headerCont = document.querySelector('.main-header')
+
+/* */
+
+/* add event on click */
+openProfile.addEventListener('click', () => {
+    profileSec.classList.remove('closeProfileAnimation')
+    profileSec.classList.add('openProfile')
+    headerCont.style.filter = 'blur(3px)'
+})
+
+closeProfile.addEventListener('click', () => {
+    profileSec.classList.add('closeProfileAnimation')
+    profileSec.classList.remove('openProfile')
+    headerCont.style.filter = 'none'
+})
+
+/* */
+
+
+/* CARDS RENDER */
+
+/* getting elements */
+
+const animesSec = document.querySelector('.m-cont-main')
+const category = document.querySelectorAll('.card-btn')
+
+const cards = cardList;
+
+/* */
+
+
+/* renderizing cards */
+
+const renderCards = () => {
+    animesSec.innerHTML = '';
+
+        cards.forEach(({ name, genre, ranking, studio, linkCard, urlCard}) => {
+            animesSec.innerHTML += `
+            <div class="banner">
+            <span class="ranking">${ranking}</span>
+
+            <div class="b-info">
+                <div class="save"></div>
+                <a href="${linkCard}" class="play" target="_blank">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAABmJLR0QA/wD/AP+gvaeTAAABiUlEQVRYhe3XsUsbYRjH8e/7XoaOYp1eccjWQfCONwYCLvoXuByUIi7i7Owgbf+Duru5GSSzQqmDFOG4Qwh0EAk4ZXQUchyvk9AhDZL3vUuH5zMePD++BA4uIIQQQgghZlI+x3EcL0VRdAHsAI/ASZ7n/SBlM0Q+x8aY70qpfUADK0BqjFk1xvwcj8dlkMIptM+xUqo95fEhkCdJsuGzPYtX9AyftNZ31tqjOsbrigb4APyw1g56vd5yyOE6o9/sTiaTe2vtVqjBJqIB1oBfnU7nW5qmXi8/NBcN0HLOfR2NRtfdbvejz1CT0W92qqq6ieN4ad6BRUQDrGut03mPFxVdRVF0P+/xIqJLpdRelmXZvAOtkDXv8KS1/pJl2W+fkcainXOXZVkeDofDZ9+tJqJfgOOiKE5DDdYd/Qf4nOf5MORobS+ic+4c2AwdDP6/9GTKs2fn3EFRFAPP7X/y/Z4+A/7+2L8F4jqDwfPvFkCSJBtKqW2t9UO73b7q9/tViDAhhBBCiP/FKyrgYUGy5bTVAAAAAElFTkSuQmCC">
+                </a>
+                <div class="b-info-wrapper">
+                    <span class="b-info-studio">${studio}</span>
+                    <span class="b-info-genre">${genre}</span>
+                    <div class="b-info-stars">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABo0lEQVQ4jcWUP2sUQRjGf+/+4VotbMZCEFRSmAgGki4WFoZDBYUQEPQObo7dTxACRk5QsLa44r3iUBT8ClaHX0AEUySI1RWCKQWrc8YiF5msu7dbCD4wxTzvM795ZwYG/oestdvW2u0m2aRJSER2AA+8q83WBay1l0XkcD5dUtWDRfmoDhhF0VYwvV+brwt470PgVmWwCTDP8yvA1cBa7vV6S4vWCIC19oWIXCypXwKuFbxPwJeS7FdV3U0AROQHx/dTewXzDYqbOODJnw4Bsiy74Zx7C5gG0FDfvfcPR6PR+1NAgG63ey5N09fArYawCfBAVb+dGKeOOB6Pj4wxbeDx/BhVciKyZ4y5GcL+6vBEWZadd85Nq+qAT5LkwnA4nBYLpY/gvb+3AAYgs9nsblmhCninYP2cj1DNgJ1O5wywEVj7wDpwHfgY+Bt5np+tBaZpehtIOf5dXrZarVVV/ayqB8aYNeAp8AtInXPt4vqy72sTmAKPVHUSFgaDwQwY9Pv9D8Ar7/0m8GZhh1EUPYvjeKUIC6WqkziOV0TkeVXmn+k31u9yotLaCb4AAAAASUVORK5CYII=" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div class="card-img"><img src="${urlCard}" alt=""></div>
+            <span class="b-name">${name}</span>
+        </div>
+            `
+        })   
+};
+
+renderCards()
+
+
+/* */
+
+/* filter cards */
+
+const renderFiltered = (genreClick) => {
+    animesSec.innerHTML = ''
+    cards.forEach(({ name, genre, ranking, studio, linkCard, urlCard}) => {
+        if(genre == genreClick){
+        animesSec.innerHTML += `
+        <div class="banner">
+        <span class="ranking">${ranking}</span>
+
+        <div class="b-info">
+            <div class="save"></div>
+            <div class="play">
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAABmJLR0QA/wD/AP+gvaeTAAABiUlEQVRYhe3XsUsbYRjH8e/7XoaOYp1eccjWQfCONwYCLvoXuByUIi7i7Owgbf+Duru5GSSzQqmDFOG4Qwh0EAk4ZXQUchyvk9AhDZL3vUuH5zMePD++BA4uIIQQQgghZlI+x3EcL0VRdAHsAI/ASZ7n/SBlM0Q+x8aY70qpfUADK0BqjFk1xvwcj8dlkMIptM+xUqo95fEhkCdJsuGzPYtX9AyftNZ31tqjOsbrigb4APyw1g56vd5yyOE6o9/sTiaTe2vtVqjBJqIB1oBfnU7nW5qmXi8/NBcN0HLOfR2NRtfdbvejz1CT0W92qqq6ieN4ad6BRUQDrGut03mPFxVdRVF0P+/xIqJLpdRelmXZvAOtkDXv8KS1/pJl2W+fkcainXOXZVkeDofDZ9+tJqJfgOOiKE5DDdYd/Qf4nOf5MORobS+ic+4c2AwdDP6/9GTKs2fn3EFRFAPP7X/y/Z4+A/7+2L8F4jqDwfPvFkCSJBtKqW2t9UO73b7q9/tViDAhhBBCiP/FKyrgYUGy5bTVAAAAAElFTkSuQmCC">
+            </div>
+            <div class="b-info-wrapper">
+                <span class="b-info-studio">${studio}</span>
+                <span class="b-info-genre">${genre}</span>
+                <div class="b-info-stars">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABo0lEQVQ4jcWUP2sUQRjGf+/+4VotbMZCEFRSmAgGki4WFoZDBYUQEPQObo7dTxACRk5QsLa44r3iUBT8ClaHX0AEUySI1RWCKQWrc8YiF5msu7dbCD4wxTzvM795ZwYG/oestdvW2u0m2aRJSER2AA+8q83WBay1l0XkcD5dUtWDRfmoDhhF0VYwvV+brwt470PgVmWwCTDP8yvA1cBa7vV6S4vWCIC19oWIXCypXwKuFbxPwJeS7FdV3U0AROQHx/dTewXzDYqbOODJnw4Bsiy74Zx7C5gG0FDfvfcPR6PR+1NAgG63ey5N09fArYawCfBAVb+dGKeOOB6Pj4wxbeDx/BhVciKyZ4y5GcL+6vBEWZadd85Nq+qAT5LkwnA4nBYLpY/gvb+3AAYgs9nsblmhCninYP2cj1DNgJ1O5wywEVj7wDpwHfgY+Bt5np+tBaZpehtIOf5dXrZarVVV/ayqB8aYNeAp8AtInXPt4vqy72sTmAKPVHUSFgaDwQwY9Pv9D8Ar7/0m8GZhh1EUPYvjeKUIC6WqkziOV0TkeVXmn+k31u9yotLaCb4AAAAASUVORK5CYII=" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="card-img"><img src="${urlCard}" alt=""></div>
+            <span class="b-name">${name}</span>
+    </div>
+        `}
+    }) 
+}
+
+//renderFiltered()
+
+for(let i = 0; i < category.length; i++){
+    category[i].addEventListener('click', ({target}) => {
+        if(target.classList.contains('liActive')){
+            target.classList.remove('liActive')
+            renderCards()
+            return
+        }
+        renderFiltered(target.textContent);
+        target.classList.add('liActive')
+
+        for(let i = 0; i < category.length; i++){
+            if(category[i] != target){
+                category[i].classList.remove('liActive')
+            }
+        }
+    })
+}
+
+/* */
+
+/* SAVE ANIME IN LIST */
+
+/*getting elementr*/
+
+const list = document.querySelector('#list')
+const saveBtn = document.querySelectorAll('.save')
+
+const myList = []
+
+/* */
+
+/* elements push */
+
+saveBtn.forEach((element, i) => {
+    element.addEventListener('click', () => {
+    //    myList.forEach((item, itemI) => {
+            
+    //    }) 
+
+       myList.push(cards.filter( (item, itemI) => {
+        if(itemI === i){
+            console.log(item);
+            return item
+        }
+       }));
+
+       console.log(myList);
+    })
+})
+
+list.addEventListener('click', () => {
+    animesSec.innerHTML = ''
+    myList.forEach(({ name, genre, ranking, studio, linkCard, urlCard}) => {
+  
+        animesSec.innerHTML += `
+        <div class="banner">
+        <span class="ranking">${ranking}</span>
+
+        <div class="b-info">
+            <div class="save"></div>
+            <div class="play">
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAC0AAAAtCAYAAAA6GuKaAAAABmJLR0QA/wD/AP+gvaeTAAABiUlEQVRYhe3XsUsbYRjH8e/7XoaOYp1eccjWQfCONwYCLvoXuByUIi7i7Owgbf+Duru5GSSzQqmDFOG4Qwh0EAk4ZXQUchyvk9AhDZL3vUuH5zMePD++BA4uIIQQQgghZlI+x3EcL0VRdAHsAI/ASZ7n/SBlM0Q+x8aY70qpfUADK0BqjFk1xvwcj8dlkMIptM+xUqo95fEhkCdJsuGzPYtX9AyftNZ31tqjOsbrigb4APyw1g56vd5yyOE6o9/sTiaTe2vtVqjBJqIB1oBfnU7nW5qmXi8/NBcN0HLOfR2NRtfdbvejz1CT0W92qqq6ieN4ad6BRUQDrGut03mPFxVdRVF0P+/xIqJLpdRelmXZvAOtkDXv8KS1/pJl2W+fkcainXOXZVkeDofDZ9+tJqJfgOOiKE5DDdYd/Qf4nOf5MORobS+ic+4c2AwdDP6/9GTKs2fn3EFRFAPP7X/y/Z4+A/7+2L8F4jqDwfPvFkCSJBtKqW2t9UO73b7q9/tViDAhhBBCiP/FKyrgYUGy5bTVAAAAAElFTkSuQmCC">
+            </div>
+            <div class="b-info-wrapper">
+                <span class="b-info-studio">${studio}</span>
+                <span class="b-info-genre">${genre}</span>
+                <div class="b-info-stars">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABpklEQVQ4jcWUQUsbURCAv3nJ0qsehGYPoSappTkJFtqbPVZECwoiCPZnaKDGFy3Sc39BLS30L/Sk/gERFKqIMcGGBS8epJ7cN73UsNms2QUPDuxhZ7755s1bWHiM6KyVFztr5cUsbD4LJKIroAr8TGXTgD+2NGYcJwAY99K3reNBvEkTmlAWui9O5lP5NADRrlCIyO/DBxU7H0dfiJHeFV1Y9T+1fw8UBvXSZ0VK/WV9DozHkgcgp/0mPfMbzVoeQNFrYJ4sVwDjoPEhDtV694QAga28xekPRf0M0mhcglv2N1q/eoQAQa0yovnwGyLvMsp2vNtwaWSrHdwl+j6KWkzgRmsgG9x/BQ6V9ULubEssLlroaxCLC0Pva9KwKKaO7bgsUQhgcuFcmhDj3if2Jo/X2Vjq5v8TMUo24bl9NoQwGUkdGeUNxk0A+xHjZHu1OJwqfOJkBvAARfTL3yvz6ulm89C3reOCKb5WaAAh4Hlebjren/D7kingQpAPhUZzp6did28BG9TLe4puY2QK+J60ejcCW6kmrRKP9mpxOLCVahr34PgHLyyDPtXSl10AAAAASUVORK5CYII=">
+                    <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAUCAYAAACNiR0NAAAABmJLR0QA/wD/AP+gvaeTAAABo0lEQVQ4jcWUP2sUQRjGf+/+4VotbMZCEFRSmAgGki4WFoZDBYUQEPQObo7dTxACRk5QsLa44r3iUBT8ClaHX0AEUySI1RWCKQWrc8YiF5msu7dbCD4wxTzvM795ZwYG/oestdvW2u0m2aRJSER2AA+8q83WBay1l0XkcD5dUtWDRfmoDhhF0VYwvV+brwt470PgVmWwCTDP8yvA1cBa7vV6S4vWCIC19oWIXCypXwKuFbxPwJeS7FdV3U0AROQHx/dTewXzDYqbOODJnw4Bsiy74Zx7C5gG0FDfvfcPR6PR+1NAgG63ey5N09fArYawCfBAVb+dGKeOOB6Pj4wxbeDx/BhVciKyZ4y5GcL+6vBEWZadd85Nq+qAT5LkwnA4nBYLpY/gvb+3AAYgs9nsblmhCninYP2cj1DNgJ1O5wywEVj7wDpwHfgY+Bt5np+tBaZpehtIOf5dXrZarVVV/ayqB8aYNeAp8AtInXPt4vqy72sTmAKPVHUSFgaDwQwY9Pv9D8Ar7/0m8GZhh1EUPYvjeKUIC6WqkziOV0TkeVXmn+k31u9yotLaCb4AAAAASUVORK5CYII=" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="card-img"><img src="${urlCard}" alt=""></div>
+            <span class="b-name">${name}</span>
+    </div>
+        `
+    }) 
+})
+
+
+/* */
+
